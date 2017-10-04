648127c3f4fd7c1e54b7aa71ce4ee30a39f38447
ecurtin2 <ecurtin2@illinois.edu>
2017-10-04 11:41:04 -0500

    Added presentation for Norskov lunch

---
+++ analysis/log/smallruns.sh
@@ -0,0 +1,22 @@
+mpirun -n 3 ../../HFS/bin/release/3d/HFS --Nk 8 --fname HFS_3d_8Nk_1-0rs.json --rs 1.0 --Dav_maxits 11 &
+wait
+mpirun -n 3 ../../HFS/bin/release/3d/HFS --Nk 8 --fname HFS_3d_8Nk_1-5rs.json --rs 1.5 --Dav_maxits 11 &
+wait
+mpirun -n 3 ../../HFS/bin/release/3d/HFS --Nk 8 --fname HFS_3d_8Nk_2-0rs.json --rs 2.0 --Dav_maxits 11 &
+wait
+mpirun -n 3 ../../HFS/bin/release/3d/HFS --Nk 8 --fname HFS_3d_8Nk_2-5rs.json --rs 2.5 --Dav_maxits 11 &
+wait
+mpirun -n 3 ../../HFS/bin/release/3d/HFS --Nk 8 --fname HFS_3d_8Nk_3-0rs.json --rs 3.0 --Dav_maxits 11 &
+wait
+mpirun -n 3 ../../HFS/bin/release/3d/HFS --Nk 8 --fname HFS_3d_8Nk_3-5rs.json --rs 3.5 --Dav_maxits 11 &
+wait
+mpirun -n 3 ../../HFS/bin/release/3d/HFS --Nk 8 --fname HFS_3d_8Nk_4-0rs.json --rs 4.0 --Dav_maxits 11 &
+wait
+mpirun -n 3 ../../HFS/bin/release/3d/HFS --Nk 8 --fname HFS_3d_8Nk_4-5rs.json --rs 4.5 --Dav_maxits 11 &
+wait
+mpirun -n 3 ../../HFS/bin/release/3d/HFS --Nk 8 --fname HFS_3d_8Nk_5-0rs.json --rs 5.0 --Dav_maxits 11 &
+wait
+mpirun -n 3 ../../HFS/bin/release/3d/HFS --Nk 8 --fname HFS_3d_8Nk_5-5rs.json --rs 5.5 --Dav_maxits 11 &
+wait
+mpirun -n 3 ../../HFS/bin/release/3d/HFS --Nk 8 --fname HFS_3d_8Nk_6-0rs.json --rs 6.0 --Dav_maxits 11 &
+wait
